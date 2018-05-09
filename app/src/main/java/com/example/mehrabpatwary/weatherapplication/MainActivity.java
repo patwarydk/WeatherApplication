f51108530e7ff2bad29215c8c415f559cf811d71
sanzanashoumy <sanzanashoumy23@gmail.com>
2018-05-09 19:53:12 +0600

    Shoumy

--- app/src/main/java/com/example/mehrabpatwary/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/mehrabpatwary/weatherapplication/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.mehrabpatwary.weatherapplication;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Current"));
+        tabLayout.addTab(tabLayout.newTab().setText("Forecast"));
+
+        TestPagerAdapter adapter = new TestPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+    private class TestPagerAdapter extends FragmentPagerAdapter {
+        private int tabCount;
+        public TestPagerAdapter(FragmentManager fm, int tabCount) {
+            super(fm);
+            this.tabCount = tabCount;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new Current();
+                case 1:
+                    return new Forecast();
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return tabCount;
+        }
+    }
+
 }
