61827ae737453e8653f4ae1646380fc7087de9c0
Mohsin Patwary <patwarydk@gmail.com>
2018-05-10 11:28:13 +0600

    Mohsin

--- app/src/main/java/com/example/mehrabpatwary/weatherapplication/Forecast.java
+++ app/src/main/java/com/example/mehrabpatwary/weatherapplication/Forecast.java
@@ -22,6 +22,7 @@ public class Forecast extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.forecast, container, false);
     }
