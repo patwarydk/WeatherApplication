038da1021946c49c885e30f0f80dd57dd5c81068
Mohsin Patwary <patwarydk@gmail.com>
2018-05-10 13:10:18 +0600

    Mohsin

--- app/src/main/java/com/example/mehrabpatwary/weatherapplication/ForecastApi.java
+++ app/src/main/java/com/example/mehrabpatwary/weatherapplication/ForecastApi.java
@@ -2,6 +2,12 @@ package com.example.mehrabpatwary.weatherapplication;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Url;
 
 public interface ForecastApi {
+
+   @GET("forecast?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22")
+
+   Call<ForecastWeatherResponse>getForecastWeather(@Url String urlString);
+
    }
